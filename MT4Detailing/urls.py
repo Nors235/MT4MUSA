6e74a22a346cb0a201dca652d4a31962bd2deaa4
Noora235 <nooraibrahim65@gmail.com>
2024-07-12 12:47:36 +0000

    created migrations

--- MT4Detailing/urls.py
+++ MT4Detailing/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
 
-    path('', include('MT4app.urls')),
+   
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
 
