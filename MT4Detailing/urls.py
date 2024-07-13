fb342ed4b97f882013de5edc226e3eefedad4654
Noora235 <nooraibrahim65@gmail.com>
2024-07-13 21:03:29 +0000

    initial commit

--- MT4Detailing/urls.py
+++ MT4Detailing/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("accounts/", include("allauth.urls")),
-    path("", include("MT4app.urls"), name="MT4app.urls"),
+    path("", include("MT4app.urls")),
     
 ]
--- MT4app/views.py
+++ MT4app/views.py
@@ -1,43 +1,48 @@
 from django.shortcuts import render, get_object_or_404, redirect 
-
 from django.views.generic import TemplateView 
-
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Service, Review
 
+
 def welcome(request):
-    return render(request,'MT4app/welcome.html')
+    """
+    Render the welcome page.
 
-
-class DetailingView(TemplateView):
-    pass
-
-class ServiceListView(ListView):
-    model = Service
-    template_name = 'services/services_list.html'
-    context_object_name = 'services'
-
-class ServiceDetailView(DetailView):
-    model = Service
-    template_name = 'services/service_detail.html'
-    context_object_name = 'service'
+    Returns the rendered 'welcome.html' template.
+    """
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['reviews'] = Review.objects.filter(service=self.object)
-        return context
+    return render(request, 'MT4app/welcome.html')
 
 
-class ReviewCreateView(LoginRequiredMixin, CreateView):
-    model = Review
-    fields = ['content', 'rating']
-    template_name = 'reviews/create_review.html'
+# class DetailingView(TemplateView):
+#     pass
+
+# class ServiceListView(ListView):
+#     model = Service
+#     template_name = 'services/services_list.html'
+#     context_object_name = 'services'
+
+# class ServiceDetailView(DetailView):
+#     model = Service
+#     template_name = 'services/service_detail.html'
+#     context_object_name = 'service'
     
-    def form_valid(self, form):
-        form.instance.service = Service.objects.get(pk=self.kwargs['service_pk'])
-        form.instance.customer = self.request.user
-        return super().form_valid(form)
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['reviews'] = Review.objects.filter(service=self.object)
+#         return context
+
+
+# class ReviewCreateView(LoginRequiredMixin, CreateView):
+#     model = Review
+#     fields = ['content', 'rating']
+#     template_name = 'reviews/create_review.html'
     
-    def get_success_url(self):
-        return reverse('services-detail', args=[self.object.service.pk])
+#     def form_valid(self, form):
+#         form.instance.service = Service.objects.get(pk=self.kwargs['service_pk'])
+#         form.instance.customer = self.request.user
+#         return super().form_valid(form)
+    
+#     def get_success_url(self):
+#         return reverse('services-detail', args=[self.object.service.pk])
